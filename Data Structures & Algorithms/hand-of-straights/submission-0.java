ab015ac2df0814ff365b6d4c576e85715e01c995
Sumukh Veeramalla <77837609+sumukhveeramalla@users.noreply.github.com>
2026-05-02 14:36:10 -0400

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) return false;
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for(int n: hand) count.put(n,1+count.getOrDefault(n,0));
+
+        PriorityQueue<Integer> minH = new PriorityQueue<>(count.keySet());
+        while(!minH.isEmpty()){
+            int first = minH.peek();
+            for(int i = first; i<first+groupSize; i++){
+                if(!count.containsKey(i)) return false;
+                count.put(i,count.get(i)-1);
+                if(count.get(i) == 0){
+                    if(i != minH.peek()) return false;
+                    minH.poll();
+                }
+            }
+        }
+        return true;
+    }
+}
